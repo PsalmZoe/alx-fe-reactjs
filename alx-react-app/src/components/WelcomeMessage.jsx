9dcb9b9501c670e43f0e9a13906f0bb54d134bc6
psalmzoe <psalmzoe007@gmail.com>
2025-09-07 14:39:07 +0100

    Learn to create and assemble multiple specific React components within a main application component.

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,12 +1,10 @@
-import WelcomeMessage from './components/WelcomeMessage.jsx';
 function WelcomeMessage() {
-    return (
-        <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
-            <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
+    </div>
+  );
 }
 
 export default WelcomeMessage;
