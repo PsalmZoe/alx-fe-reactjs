2c2d5bdfc76b2d3a98580fa6734c19e1e91a1c8d
psalmzoe <psalmzoe007@gmail.com>
2025-09-08 00:29:31 +0100

    Refactor an existing React application that uses prop drilling to manage user data across multiple components.

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -17,16 +17,4 @@ function App() {
   );
 }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      <Counter />
-      <Footer />
-    </div>
-  );
-}
-
 export default App;
