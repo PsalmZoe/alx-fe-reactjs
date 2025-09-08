82cbf7912bbca760f2e88d035f39b098b8617b4f
psalmzoe <psalmzoe007@gmail.com>
2025-09-08 18:02:24 +0100

    Enhance the Recipe Sharing Application with advanced search and filtering capabilities, allowing users to easily locate recipes based on specific criteria such as ingredients, recipe name, or preparation time.

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useRecipeStore } from "../recipeStore.js";
 
 const RecipeList = () => {
@@ -11,7 +12,9 @@ const RecipeList = () => {
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
