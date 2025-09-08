132fff1f48281148fd6b25f244c6c8327d1c8ab9
psalmzoe <psalmzoe007@gmail.com>
2025-09-08 18:08:20 +0100

    Enhance the user experience in your Recipe Sharing Application by adding functionality for users to favorite recipes and receive personalized recommendations based on their preferences and interactions.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,8 @@ import AddRecipeForm from "./components/AddRecipeForm.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import RecipeList from "./components/RecipeList.jsx";
 import RecipeDetails from "./components/RecipeDetails.jsx";
+import FavoritesList from "./components/FavoritesList.jsx";
+import RecommendationsList from "./components/RecommendationsList.jsx";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
         </Routes>
       </div>
     </Router>
