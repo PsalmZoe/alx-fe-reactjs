57fe0294861d8bef8433b523e28909b7133d11ca
psalmzoe <psalmzoe007@gmail.com>
2025-09-08 17:33:23 +0100

    Start building a Recipe Sharing Application with React and Zustand, setting up the initial state management for recipes.

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,5 +4,16 @@ export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === updatedRecipe.id ? { ...r, ...updatedRecipe } : r
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({ recipes: state.recipes.filter((r) => r.id !== id) })),
+
   setRecipes: (recipes) => set({ recipes }),
 }))
